08c167003050702eb2a2dbec5a67b1b99f99e8dc
mandos1995 <mandos19950620@gmail.com>
2022-04-25 16:44:54 +0900

    🎉 프로젝트 시작

---
+++ FindMiddlePoint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindMiddlePoint
+//
+//  Created by 김민석 on 2022/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
