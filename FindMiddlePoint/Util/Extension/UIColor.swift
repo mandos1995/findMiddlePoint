a38bb2cfd9e7bfe602e184da550b03a8f54b5012
mandos1995 <mandos19950620@gmail.com>
2022-04-25 17:03:12 +0900

    ✨ UIViewController, UIColor Extension 추가

---
+++ FindMiddlePoint/Util/Extension/UIColor.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor.swift
+//  FindMiddlePoint
+//
+//  Created by 김민석 on 2022/04/25.
+//
+
+import UIKit
+
+extension UIColor {
+    // MARK: hex code를 이용하여 정의
+    // ex. UIColor(hex: 0xF5663F)
+    convenience init(hex: UInt, alpha: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(hex & 0x0000FF) / 255.0,
+            alpha: CGFloat(alpha)
+        )
+    }
+}
---
+++ FindMiddlePoint/Util/Extension/UIViewController.swift.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController.swift.swift
+//  FindMiddlePoint
+//
+//  Created by 김민석 on 2022/04/25.
+//
+
+import UIKit
+
+extension UIViewController {
+    // MARK: 빈 화면을 눌렀을 때 키보드가 내려가도록
+    func dismissKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer =
+            UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(false)
+    }
+}
