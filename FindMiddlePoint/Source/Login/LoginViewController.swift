599a6db163987bb44e891eb53dd5460e24fb2b1a
mandos1995 <mandos19950620@gmail.com>
2022-04-26 14:29:21 +0900

    💄 로그인 화면 추가

---
+++ FindMiddlePoint/Source/Login/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  FindMiddlePoint
+//
+//  Created by 김민석 on 2022/04/26.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
