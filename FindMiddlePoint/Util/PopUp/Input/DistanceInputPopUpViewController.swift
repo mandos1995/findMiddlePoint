3e545f4cf0c245e7048248a354181bd9144c4eef
mandos1995 <mandos19950620@gmail.com>
2022-04-28 23:30:46 +0900

    💄 주소 입력창 화면 구현

---
+++ FindMiddlePoint/Util/PopUp/Input/DistanceInputPopUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DistanceInputPopUpViewController.swift
+//  FindMiddlePoint
+//
+//  Created by 김민석 on 2022/04/28.
+//
+
+import UIKit
+
+class DistanceInputPopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
